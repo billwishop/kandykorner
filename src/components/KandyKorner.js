463df82e05add56fc7ce1eb95dade2eda8baaa00
billwishop <willbishop.hello@gmail.com>
2020-12-02 11:08:03 -0600

    Added navbar

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { ApplicationViews } from "./ApplicationView"
+import { NavBar } from "./nav/NavBar"
 
 export const KandyKorner = () => (
     <>
+        <NavBar />
         <ApplicationViews />
     </>
 )
